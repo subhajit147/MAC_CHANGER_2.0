3d45d27e7b705704a3f77f5112c5894b7d7c8093
subhajit147 <142143956+subhajit147@users.noreply.github.com>
2023-08-16 10:47:58 -0400

    Add files via upload

---
+++ LINUX_MAC_CHANGER.sh
@@ -0,0 +1,287 @@
+#!/bin/bash
+
+echo "CREATED BY SUBHAJIT"
+
+sudo apt install macchanger -y
+
+sudo macchanger -a eth0
+sudo macchanger -a eth0
+
+echo "Hi users your MAC_ADDRESS will be changed automatically"
+
+echo "On progress"
+echo "On progress"
+echo "On progress"
+echo "On progress"
+echo "On progress please wait"
+
+sleep 10
+
+sudo macchanger -a eth0
+sudo macchanger -a eth0
+
+echo "Hi users your MAC_ADDRESS will be changed automatically"
+
+echo "On progress"
+echo "On progress"
+
+
+sleep 10
+
+echo "On progress"
+echo "On progress"
+echo "On progress"
+echo "On progress"
+echo "On progress please wait"
+
+
+sleep 25
+
+sudo macchanger -a eth0
+sudo macchanger -a eth0
+sudo macchanger -a eth0
+
+echo "On progress"
+echo "On progress please wait"
+
+sleep 10
+
+echo "Hi users your MAC_ADDRESS will be changed automatically"
+echo "On progress"
+echo "On progress please wait"
+
+
+sleep 55
+
+sudo macchanger -a eth0
+sudo macchanger -a eth0
+sudo macchanger -a eth0
+sudo macchanger -a eth0
+
+echo "On progress please wait"
+echo "On progress please wait"
+echo "On progress please wait"
+echo "On progress please wait"
+echo "On progress please wait"
+
+
+sleep 20
+
+sudo macchanger -a eth0
+sudo macchanger -a eth0
+sudo macchanger -a eth0
+sudo macchanger -a eth0
+sudo macchanger -a eth0
+
+
+echo "Hi users your MAC_ADDRESS will be changed automatically"
+echo "On progress please wait"
+echo "On progress please wait"
+
+sleep 10
+
+echo "On progress"
+echo "On progress please wait"
+echo "On progress please wait"
+
+
+sleep 60
+
+sudo macchanger -a eth0
+sudo macchanger -a eth0
+sudo macchanger -a eth0
+
+echo "Hi users your MAC_ADDRESS will be changed automatically please be present"
+echo "On progress please wait"
+echo "On progress please wait"
+echo "On progress please wait"
+echo "On progress please wait"
+echo "On progress please wait"
+echo "On progress please wait"
+
+
+sleep 60
+
+
+sudo macchanger -a eth0
+sudo macchanger -a eth0
+sudo macchanger -a eth0
+sudo macchanger -a eth0
+
+echo "Hi users your MAC_ADDRESS will change auto and it will take some time to update a new MAC_ADDRESS"
+echo "On progress please wait"
+echo "On progress please wait"
+echo "On progress please wait"
+echo "On progress please wait"
+echo "On progress please wait"
+
+sleep 20
+
+echo "On progress"
+echo "On progress please wait"
+echo "On progress please wait"
+echo "On progress please wait"
+echo "On progress please wait"
+
+
+sleep 60
+sudo macchanger -a eth0
+sudo macchanger -a eth0
+sudo macchanger -a eth0
+
+echo "Please wait"
+
+echo "On progress"
+
+echo "On progress please wait"
+
+
+sleep 60
+
+sudo macchanger -a eth0
+sudo macchanger -a eth0
+sudo macchanger -a eth0
+sudo macchanger -a eth0
+sudo macchanger -a eth0
+sudo macchanger -a eth0
+sudo macchanger -a eth0
+sudo macchanger -a eth0
+
+echo "On progress"
+
+echo "On progress"
+
+echo "On progress"
+
+echo "On progress please wait"
+
+sleep 60
+
+sudo macchanger -a eth0
+sudo macchanger -a eth0
+sudo macchanger -a eth0
+sudo macchanger -a eth0
+sudo macchanger -a eth0
+
+echo "On progress"
+
+echo "On progress"
+
+echo "On progress please wait"
+
+sleep 60
+
+sudo macchanger -a eth0
+sudo macchanger -a eth0
+sudo macchanger -a eth0
+sudo macchanger -a eth0
+sudo macchanger -a eth0
+
+
+echo "On progress"
+
+echo "On progress please wait"
+
+sudo macchanger -a eth0
+sudo macchanger -a eth0
+sudo macchanger -a eth0
+sudo macchanger -a eth0
+sudo macchanger -a eth0
+
+
+
+sleep 60
+
+echo "On progress"
+
+echo "On progress please wait"
+
+sudo macchanger -a eth0
+sudo macchanger -a eth0
+sudo macchanger -a eth0
+sudo macchanger -a eth0
+sudo macchanger -a eth0
+
+
+echo "On progress"
+echo "On progress"
+echo "On progress"
+echo "On progress"
+echo "On progress"
+echo "On progress"
+echo "On progress"
+echo "On progress"
+echo "On progress"
+echo "On progress please wait"
+
+sleep  60
+sudo macchanger -a eth0
+sudo macchanger -a eth0
+sudo macchanger -a eth0
+sudo macchanger -a eth0
+sudo macchanger -a eth0
+
+echo "On progress"
+echo "On progress"
+echo "On progress"
+echo "On progress"
+echo "On progress"
+echo "On progress"
+echo "On progress please wait"
+
+
+sleep 60
+
+sudo macchanger -a eth0
+sudo macchanger -a eth0
+sudo macchanger -a eth0
+sudo macchanger -a eth0
+
+
+echo "On progress"
+echo "On progress"
+echo "On progress"
+echo "On progress"
+echo "On progress please wait"
+
+sudo macchanger -a eth0
+
+sleep 60
+
+echo "On progress"
+echo "On progress"
+echo "On progress"
+echo "On progress"
+echo "On progress please wait"
+
+
+sudo macchanger -a eth0
+sudo macchanger -a eth0
+sudo macchanger -a eth0
+sudo macchanger -a eth0
+sudo macchanger -a eth0
+sudo macchanger -a eth0
+sudo macchanger -a eth0
+
+echo "On progress"
+echo "On progress"
+echo "On progress"
+echo "On progress"
+echo "On progress"
+echo "On progress"
+echo "On progress"
+echo "On progress"
+echo "On progress"
+echo "On progress please wait"
+echo "On progress please wait"
+echo "On progress please wait"
+echo "On progress please wait"
+
+sleep  90
+sudo macchanger -a eth0
+sudo macchanger -a eth0
+sudo macchanger -a eth0
+sudo macchanger -a eth0
+sudo macchanger -a eth0
+
+echo "THANK YOU SO MUCH USERS"
